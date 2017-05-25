8c061ed47ef570859708045663ff9e3bfa89b2ab
riquet28 <eric.merlin3@gmail.com>
2017-05-25 18:17:21 +0200

    Section 8.3 ok, évènements formulaire

--- js/script.js
+++ js/script.js
@@ -1,56 +1,22 @@
 $(document).ready(function(){
 
-	// alert("Hello World");
-	// console.log("test");
+	var form = $('#form'), choix = $('#choix'), sujet = $('#sujet');
 
-	// $("#paragraphe").css('color', 'red');
-	// $("#paragraphe").text('texte de remplacement');
-
-	// $("#paragraphe").on('click', function(){
-	// 	$(this).css('font-weight', 'bold');
-	// });
-
-	// $('.article').each(function(){
-	// 	$(this).on('click', function(){
-	// 		// alert('ok!!!');
-	// 		$(this).css('color', 'blue');
-	// 		alert($(this).css('color'));
-	// 	});
-	// });
-
-	// $('#button').on('click', function(){
-	// 	$('.article').width(400).css('color', 'blue');
-	// });
-
-	/*
-	var paragraphe = $('#paragraphe'); article = $('.article');
-	button = $('#button');
-
-	$(paragraphe).on('click', function(){
-		$(this).css('font-weight', 'bold');
+	$(form).on('submit', function(e){
+		e.preventDefault();
+		alert('Formulaire posté');
 	});
 
-	$(article).each(function(){
-		$(this).on('click', function(){
-			// alert('ok!!!');
-			$(this).css('color', 'blue');
-			alert($(this).css('color'));
-		});
+	$(sujet).on('focus', function(){
+		console.log('Hey, g le focus');
 	});
 
-	$(button).on('click', function(){
-		$('.article').width(400).css('color', 'blue');
+	$(sujet).on('blur', function(){
+		console.log('Je suis sorti');
 	});
 
-	// $('p, div, span').css('color', 'blue');
-	$('*').css('font-weight', 'bold');
-	*/
-
-
-	$('p').parent('#parent').css('color', 'blue');
-
-	$('#autre').children().css('color', 'red');
-
-	$('#paraph').parent().css('color', '#999');
+	$(choix).change(function(){
+		console.log($(this).val());
+	});
 
 });
