faa36efbd23e4e01c09fa85317d7ccc589b41879
riquet28 <eric.merlin3@gmail.com>
2017-05-25 16:59:56 +0200

    Section 8.1 ok, évènements sur la souris

--- js/script.js
+++ js/script.js
@@ -1,56 +1,22 @@
 $(document).ready(function(){
 
-	// alert("Hello World");
-	// console.log("test");
-
-	// $("#paragraphe").css('color', 'red');
-	// $("#paragraphe").text('texte de remplacement');
-
-	// $("#paragraphe").on('click', function(){
-	// 	$(this).css('font-weight', 'bold');
-	// });
-
-	// $('.article').each(function(){
-	// 	$(this).on('click', function(){
-	// 		// alert('ok!!!');
-	// 		$(this).css('color', 'blue');
-	// 		alert($(this).css('color'));
-	// 	});
-	// });
-
-	// $('#button').on('click', function(){
-	// 	$('.article').width(400).css('color', 'blue');
-	// });
+	var image = $('#image');
 
 	/*
-	var paragraphe = $('#paragraphe'); article = $('.article');
-	button = $('#button');
-
-	$(paragraphe).on('click', function(){
-		$(this).css('font-weight', 'bold');
+	$(image).on('click', function(){
+		alert("ihoihi");
 	});
+	*/
 
-	$(article).each(function(){
-		$(this).on('click', function(){
-			// alert('ok!!!');
-			$(this).css('color', 'blue');
-			alert($(this).css('color'));
-		});
+	/*
+	$(image).on('mouseenter', function(){
+		alert("Tu rentres sur un élément");
 	});
-
-	$(button).on('click', function(){
-		$('.article').width(400).css('color', 'blue');
-	});
-
-	// $('p, div, span').css('color', 'blue');
-	$('*').css('font-weight', 'bold');
 	*/
 
 
-	$('p').parent('#parent').css('color', 'blue');
-
-	$('#autre').children().css('color', 'red');
-
-	$('#paraph').parent().css('color', '#999');
+	$(image).on('mouseout', function(){
+		alert("Tu sors d'un élément");
+	});
 
 });
