8ab3cc5e4510756bdb6d625ef138e4bd90b87c5a
riquet28 <eric.merlin3@gmail.com>
2017-05-25 16:30:47 +0200

    Section 7.4 ok, créer une fiche produit

--- js/script.js
+++ js/script.js
@@ -1,56 +1,14 @@
 $(document).ready(function(){
 
-	// alert("Hello World");
-	// console.log("test");
+	var img = $('.img-thumbnail'), large = $('#large');
 
-	// $("#paragraphe").css('color', 'red');
-	// $("#paragraphe").text('texte de remplacement');
-
-	// $("#paragraphe").on('click', function(){
-	// 	$(this).css('font-weight', 'bold');
-	// });
-
-	// $('.article').each(function(){
-	// 	$(this).on('click', function(){
-	// 		// alert('ok!!!');
-	// 		$(this).css('color', 'blue');
-	// 		alert($(this).css('color'));
-	// 	});
-	// });
-
-	// $('#button').on('click', function(){
-	// 	$('.article').width(400).css('color', 'blue');
-	// });
-
-	/*
-	var paragraphe = $('#paragraphe'); article = $('.article');
-	button = $('#button');
-
-	$(paragraphe).on('click', function(){
-		$(this).css('font-weight', 'bold');
-	});
-
-	$(article).each(function(){
-		$(this).on('click', function(){
-			// alert('ok!!!');
-			$(this).css('color', 'blue');
-			alert($(this).css('color'));
+	$(img).each(function(){
+		$(this).on('click', function(e){
+			e.preventDefault();
+			var src = $(this).attr('src');
+			$(large).attr('src', src);
+			$(large).parent().attr('href', src);
 		});
 	});
-
-	$(button).on('click', function(){
-		$('.article').width(400).css('color', 'blue');
-	});
-
-	// $('p, div, span').css('color', 'blue');
-	$('*').css('font-weight', 'bold');
-	*/
-
-
-	$('p').parent('#parent').css('color', 'blue');
-
-	$('#autre').children().css('color', 'red');
-
-	$('#paraph').parent().css('color', '#999');
 
 });
